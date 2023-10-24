486c442ddea0de3780229911745cc5e36689a68d
Justin Wei <jwei@lcs.on.ca>
2023-10-24 11:31:41 -0400

    Initial Commit

---
+++ GenerativeArt/GenerativeArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerativeArtApp.swift
+//  GenerativeArt
+//
+//  Created by Justin Zack Wei on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct GenerativeArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
