2950488b734cfdb4c7df252b55f7f7092e2e71ff
Justin Wei <jwei@lcs.on.ca>
2023-10-24 11:37:20 -0400

    got a basic shape - a square - to draw.

--- GenerativeArt/ContentView.swift
+++ GenerativeArt/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Rectangle()
+                .aspectRatio(1.0, contentMode: .fit)
+                .padding(100)
         }
         .padding()
     }
