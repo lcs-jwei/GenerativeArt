6ce4bd8acfe27f5354b39443d99cc482b6832f9a
Justin Wei <jwei@lcs.on.ca>
2023-10-24 11:44:52 -0400

    organized files and groups in the project

---
+++ GenerativeArt/Views/MosaicView.swift
@@ -0,0 +1,18 @@
+//
+//  MosaicView.swift
+//  GenerativeArt
+//
+//  Created by Justin Zack Wei on 2023-10-24.
+//
+
+import SwiftUI
+
+struct MosaicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MosaicView()
+}
---
+++ GenerativeArt/Views/TileView.swift
@@ -0,0 +1,18 @@
+//
+//  TileView.swift
+//  GenerativeArt
+//
+//  Created by Justin Zack Wei on 2023-10-24.
+//
+
+import SwiftUI
+
+struct TileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TileView()
+}
